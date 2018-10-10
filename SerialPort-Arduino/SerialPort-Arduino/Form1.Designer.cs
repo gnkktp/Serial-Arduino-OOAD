8f208be8c8a53a96badba2207e8381e51d78bd67
gnkktp <nattawanktp@gmail.com>
2018-10-10 09:48:29 +0700

    listserialport

---
+++ SerialPort-Arduino/SerialPort-Arduino/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SerialPort_Arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.ListSerialPort = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ListSerialPort
+            // 
+            this.ListSerialPort.Location = new System.Drawing.Point(62, 26);
+            this.ListSerialPort.Name = "ListSerialPort";
+            this.ListSerialPort.Size = new System.Drawing.Size(148, 58);
+            this.ListSerialPort.TabIndex = 0;
+            this.ListSerialPort.Text = "List Serialport";
+            this.ListSerialPort.UseVisualStyleBackColor = true;
+            this.ListSerialPort.Click += new System.EventHandler(this.ListSerialPort_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(62, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(148, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.ListSerialPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button ListSerialPort;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ SerialPort-Arduino/SerialPort-Arduino/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+namespace SerialPort_Arduino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListSerialPort_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            listBox1.Items.AddRange(ports);
+        }
+    }
+}
